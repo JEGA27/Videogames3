95dd9555bb3ac46d81c7738b5dc575af73b6246a
Daniel <danielttgb44@live.com.mx>
2022-08-18 18:17:45 -0500

    Health recover after time & player dies below 0

--- Videogame/Animal Shooter/Assets/Scripts/Health.cs
+++ Videogame/Animal Shooter/Assets/Scripts/Health.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     public float hp;
+    private float maxHp;
+    public float hpIncrease;
 
     public float maxFallVel;
     public float hpFallDamage;
     private float lstFrameYVel;
+
+    public float timeToRecover;
+    private float timer;
+    
 
     private Rigidbody rb;
 
@@ -16,25 +22,65 @@ public class Health : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        maxHp = hp;
+        timer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if ((-rb.velocity.y) > maxFallVel)
         {
             lstFrameYVel = rb.velocity.y;
         }
-        
+
         if (rb.velocity.y == 0 && (-lstFrameYVel) > maxFallVel)
         {
-            hp -= FallDamage(lstFrameYVel, maxFallVel, hpFallDamage);
+            hp -= (((-lstFrameYVel) - (maxFallVel)) * hpFallDamage);
             lstFrameYVel = 0;
+        }
+
+        if (hp < maxHp)
+        {
+            if (hp > 0)
+            {
+                Recover();
+            }
+            else
+            {
+                Eliminate();
+            }
         }
     }
 
-    public float FallDamage(float yVel, float maxVel, float hpLoss)
+    void Recover()
     {
-        return (((-yVel) - (maxVel)) * hpLoss);
+        timer += Time.deltaTime;
+        if (timer > timeToRecover) 
+        {
+            timer = 0f;
+            HealthRecover();
+        }
     }
+
+    void HealthRecover()
+    {
+        while (hp < maxHp)
+        {
+            hp += hpIncrease;
+            if (hp > maxHp)
+            {
+                hp = maxHp;
+            }
+        }
+        
+    }
+
+    void Eliminate()
+    {
+        Debug.Log("Dead");
+        Destroy(this.gameObject);
+    }
+
 }
