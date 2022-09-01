367b13e874e0695dcbe33ebec2fe84691995fbbe
Daniel <danielttgb44@live.com.mx>
2022-09-01 17:23:31 -0500

    Added Ability to pick up dumpster

---
+++ Videogame/Animal Shooter/Assets/Scripts/Abilities/PickUpDumpster.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpDumpster : MonoBehaviour
+{
+    public GameObject dumpsterPos;
+
+    private bool canPickUp;
+    private GameObject Dumpster; 
+    private bool hasDumpster; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canPickUp = false; 
+        hasDumpster = false;
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if(canPickUp) 
+        {
+            if (Input.GetKey("g"))  
+            {
+                Dumpster.GetComponent<Rigidbody>().isKinematic = true; 
+                Dumpster.transform.parent = gameObject.transform;
+                Dumpster.transform.position = dumpsterPos.transform.position;
+                hasDumpster = true;
+            }
+        }
+        if (Input.GetKey("e") && hasDumpster) 
+        {
+            Debug.Log("Drop");
+            Dumpster.GetComponent<Rigidbody>().isKinematic = false;
+            Dumpster.transform.parent = null;
+            hasDumpster = false;
+        }
+    }
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Dumpster")) 
+        {
+            canPickUp = true;  
+            Dumpster = other.gameObject;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        canPickUp = false;
+    }
+}
