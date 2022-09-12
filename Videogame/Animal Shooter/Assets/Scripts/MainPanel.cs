f33a1aec7927ae6a1a700e878d739ec55b69c6c0
Jesús Javier Hernández Delgado <a01658891@itesm.mx>
2022-09-11 21:30:53 -0500

    Main Menu sound

--- Videogame/Animal Shooter/Assets/Scripts/MainPanel.cs
+++ Videogame/Animal Shooter/Assets/Scripts/MainPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -10,6 +11,10 @@ public class MainPanel : MonoBehaviour
     public Slider volumeFX;
     public Slider volumeFilter;
     public Toggle mute;
+    public AudioMixer Mixer;
+    public AudioSource FxSource;
+    public AudioClip clicSound;
+    private float lastVolume;
     [Header("Panels")]
     public GameObject mainPanel;
     public GameObject optionsPanel;
@@ -21,6 +26,32 @@ public class MainPanel : MonoBehaviour
         levelselectPanel.SetActive(false);
 
         panel.SetActive(true);
+        PlaySoundButton();
+    }
+
+    private void Awake() {
+        volumeFX.onValueChanged.AddListener(ChangeVolumeFx);
+        volumeFilter.onValueChanged.AddListener(ChangeVolumeMaster);
+    }
+
+    public void setMute() {
+        if(mute.isOn) {
+            Mixer.GetFloat("VolMaster", out lastVolume);
+            Mixer.SetFloat("VolMaster", -80);
+        }
+        else 
+            Mixer.SetFloat("VolMaster", lastVolume);
+    }
+
+    public void ChangeVolumeMaster (float x) {
+        Mixer.SetFloat("VolMaster", x);
+    }
+    public void ChangeVolumeFx (float x) {
+        Mixer.SetFloat("VolFX", x);
+    }
+
+    public void PlaySoundButton() {
+        FxSource.PlayOneShot(clicSound);
     }
 
     public void PlayLevel(string levelName) {
