842e551c8a6889bd2c98f69fc1335b10b4d35e88
JEGA27 <nmokey.jega@gmail.com>
2022-10-13 16:11:14 -0500

    Rat/UI/Sewer prefabs

--- Videogame/Animal Shooter/Assets/Scripts/Abilities/Rat/SewerTeleport.cs
+++ Videogame/Animal Shooter/Assets/Scripts/Abilities/Rat/SewerTeleport.cs
@@ -10,8 +10,15 @@ public class SewerTeleport : MonoBehaviour
 
     [SerializeField]
     List<GameObject> buttons;
+    [SerializeField]
+    GameObject sewersCanvas;
+    [SerializeField]
+    GameObject abilityMessage;
 
     List<Vector3> sewersPositions;
+    ThirdPersonShooterController tpsc;
+    bool isOnSewer = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,16 +26,54 @@ public class SewerTeleport : MonoBehaviour
         {
             buttons[i].GetComponent<SewerButton>().sewerCoordinates = sewers[i].transform.position;
         }
+        sewersCanvas.SetActive(false);
+        abilityMessage.SetActive(false);
+        tpsc = gameObject.GetComponent<ThirdPersonShooterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+      if(isOnSewer && Input.GetKeyDown(KeyCode.G))
+      {
+         tpsc.enabled = false;
+         sewersCanvas.SetActive(true);
+         Cursor.lockState= CursorLockMode.None;
+      }
     }
 
     public void TeleportToSewer(Vector3 coordinates)
     {
         gameObject.transform.position = coordinates;
+        sewersCanvas.SetActive(false);
+        tpsc.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Sewer")
+        {
+
+            abilityMessage.SetActive(true);
+            isOnSewer = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+      if(other.gameObject.tag == "Sewer")
+      {
+
+          abilityMessage.SetActive(false);
+          sewersCanvas.SetActive(false);
+          isOnSewer = false;
+          tpsc.enabled = true;
+          Cursor.lockState = CursorLockMode.Locked;
+
+      }
+    }
+
+
 }
