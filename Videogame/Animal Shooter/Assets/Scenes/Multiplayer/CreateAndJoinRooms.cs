f1c5c037dea1b6a6693ef9d42ddb7da4d6155f23
diogenes1027 <a01653251@itesm.mx>
2022-10-26 18:50:12 -0500

    join and create rooms function added

---
+++ Videogame/Animal Shooter/Assets/Scenes/Multiplayer/CreateAndJoinRooms.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public InputField createInput;
+    public InputField joinInput;
+
+    // Start is called before the first frame update
+
+    public void CreateRoom() {
+
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Player local = PhotonNetwork.LocalPlayer;
+
+        //int number = PhotonNetwork.PlayerList.Count;
+        //PhotonNetwork.LocalPlayer.NickName = name.text;
+
+        PhotonNetwork.LoadLevel("City");
+    }
+}
