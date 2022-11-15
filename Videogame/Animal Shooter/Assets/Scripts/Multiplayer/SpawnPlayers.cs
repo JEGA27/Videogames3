337a16262a4c6e492ee644f6951a7de3ea687cf4
EdgarRostro <a01029036@itesm.mx>
2022-11-14 20:59:01 -0600

    fix spawner3

--- Videogame/Animal Shooter/Assets/Scripts/Multiplayer/SpawnPlayers.cs
+++ Videogame/Animal Shooter/Assets/Scripts/Multiplayer/SpawnPlayers.cs
@@ -43,7 +43,8 @@ public class SpawnPlayers : MonoBehaviour
         //         PhotonNetwork.CurrentRoom.SetCustomProperties(hash);
         //     }
         // }
-        if (PhotonNetwork.IsMasterClient) //(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        
+        if (GameObject.FindGameObjectWithTag("MapManager") == null) //(PhotonNetwork.CurrentRoom.PlayerCount == 1)
         {
             GameObject mapManager = PhotonNetwork.Instantiate("MapManager", Vector3.zero, Quaternion.identity);
             mapManager.name = "MapManager";
